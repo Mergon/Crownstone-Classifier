0941c10ccf2c5909ee5dc5e13b9d6b654b399ec6
Mergon <merijn@siemensz.com>
2015-03-20 14:15:56 +0100

    Repo Init

---
+++ Crownstone Classifier/Crownstone Classifier/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Crownstone Classifier
+//
+//  Created by Meri van Tooren on 20/03/15.
+//  Copyright (c) 2015 DoBots. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
